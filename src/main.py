48d93bf6b14421f9a568f8851531e43f859ee976
Yaroslav Borkov <yarikichek.bro2006@gmail.com>
2026-09-22 02:13:57 +0300

    Add CLI command for creating tasks

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
+import sys
+
+from storage import add_task
+
+
 def main():
-    print("Task Tracker CLI")
+    if len(sys.argv) < 3 or sys.argv[1] != "add":
+        print("Usage: python3 src/main.py add <task>")
+        return
+
+    title = " ".join(sys.argv[2:])
+    task = add_task(title)
+
+    print(f"Task added: {task['title']}")
 
 
 if __name__ == "__main__":
